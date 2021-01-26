8d9e9b56395ffa3e163d364abab3ceeabeae5d4f
Ishwar Baisla <ishwar2303@gmail.com>
2021-01-26 23:25:04 +0530

    updated

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
     else{
         $login_time = $_SESSION['login_time'];
         $current_time = time();
-        if($current_time - $login_time > 300){
+        $left_seconds = 900 - ($current_time - $login_time); 
+        if($current_time - $login_time > 900){
             header('Location: logout.php');
         }
     }
@@ -20,7 +21,20 @@
     <link rel="stylesheet" href="public/css/header.css">
     <link rel="stylesheet" href="public/css/common.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <style>
+        #timer{
+            color: green;
+            font-weight: 600;
+            font-size: 30px;
+            background: rgba(0,255,0,0.2);
+            padding: 10px 20px;
+            border-radius: 10px;
+            margin: 10px;
+            position: absolute;
+            right: 0px;
+            top: 50px;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -89,5 +103,48 @@
         ?>
         </table>
     </div>
+
+    <div id="timer"><?php echo $left_seconds; ?></div>
+
+
+    <script>
+        let seconds = <?php echo $left_seconds; ?>;
+        function timer(){
+            console.log('Seconds left : ' + seconds)
+            let hour = parseInt(seconds/(3600))
+            let timeLeft = seconds - hour*3600
+
+            let minute = parseInt(timeLeft/60)
+            timeLeft = timeLeft - minute*60
+
+            let second = timeLeft
+            
+            hour = hour.toString()
+            minute = minute.toString()
+            second = second.toString()
+            if(hour.length == 1)
+                hour = '0' + hour
+            if(minute.length == 1)
+                minute = '0' + minute
+            if(second.length == 1)
+                second = '0' + second
+            
+            let watch = hour + ' : ' + minute + ' : ' + second
+
+            let timerElement = document.getElementById('timer')
+            timerElement.style.display = 'block'
+            timerElement.innerHTML = watch
+            seconds--
+            console.log(watch)
+            if(seconds < 60){
+                timerElement.style.color = 'red'
+                timerElement.style.background = 'rgba(255,0,0,0.2)'
+                setTimeout(() => {timerElement.style.display = 'none'},600)
+            }
+            if(seconds < 0)
+                location.href = 'logout.php'
+        }
+        setInterval(timer, 1000)
+    </script>
 </body>
 </html>
